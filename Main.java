7f0e57a841d4a93949a970173007779d48717312
Andrew Ohwoka <150045330+AndrewOhwoka@users.noreply.github.com>
2024-12-09 19:47:08 -0330

    UpdateMore
    More

--- Main.java
+++ Main.java
@@ -4,11 +4,12 @@ public class Main {
         AnimalShelter shelter = new AnimalShelter();
 
         // Add animals to the shelter
+        
         shelter.enqueue(new Dog("Dog1"));
         shelter.enqueue(new Cat("Cat1"));
         shelter.enqueue(new Dog("Dog2"));
         shelter.enqueue(new Cat("Cat2"));
-
+       
         // Dequeue animals
         System.out.println("Dequeued Any: " + shelter.dequeueAny().getName()); // Dog1
         System.out.println("Dequeued Cat: " + shelter.dequeueCat().getName()); // Cat1
